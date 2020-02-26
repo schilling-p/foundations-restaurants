ff8388e9ece1ebf1f24bd64657cbd6c5282bd6ca
schilling-p <pschilli97@googlemail.com>
2020-02-26 15:27:50 +0100

    Third commit

--- cgi-bin/server.py
+++ cgi-bin/server.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 file = open("restaurants.txt", "r")
 restaurants = list(file)
+file.close()
 
-
+# this function takes care of removing the "\n" at the end of each list item
 def strip_list(liste):
     new_list = []
     for i in liste:
@@ -9,16 +12,22 @@ def strip_list(liste):
         new_list.append(i)
     return new_list
 
+# this will be the list to work with:
 list_of_resturants = strip_list(restaurants)
 
-print(list_of_resturants)
-
-
-
-# string1 = "Alt-Treptow\n"
-# string1 = string1[:-1]
-# print(string1)
-
-
-
-file.close()
+website = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Restaurants in Berlin</title>
+    </head>
+    <body>
+        <h1>This is a website of restaurants in Berlin</h1>
+        <p>There are numerous:</p>
+        <p>{list_of_resturants[0]}</p>
+        <p>{list_of_resturants[1]}</p>
+        <p>{list_of_resturants[2]}</p>
+    </body>
+</html>
+"""
+print(website)
